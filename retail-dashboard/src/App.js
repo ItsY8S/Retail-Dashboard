1ca52c902a544239603e502941951606120391af
ItsY8S <gri.y8s@gmail.com>
2019-02-14 12:06:00 -0500

    Fetched data from Google Sheet

--- retail-dashboard/src/App.js
+++ retail-dashboard/src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import config from './config'
+
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${
+  config.spreadsheetId
+}/values:batchGet?ranges=Sheet1&majorDimension=ROWS&key=${config.apiKey}`
 
 class App extends Component {
+  state = {
+    items: []
+  }
+
+  async componentDidMount() {
+    let response = await fetch(url)
+    let data = await response.json()
+    let batchRowValues = await data.valueRanges[0].values
+
+    const rows = []
+
+    for (let i = 1; i < batchRowValues.length; i++) {
+      let rowObject = {}
+      for (let j = 0; j < batchRowValues[i].length; j++) {
+        rowObject[batchRowValues[0][j]] = batchRowValues[i][j]
+      }
+      rows.push(rowObject)
+    }
+    this.setState({ items: rows })
+    console.log(this.state)
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div />
   }
 }
 
-export default App;
+export default App
